50f52b3939551ff6cf77f07c7e2e291b8382e45f
Michael Song <Michael@Michaels-MacBook-Pro.local>
2016-03-11 17:09:26 -0500

    Signup servlet without call to DB controller

--- MentorMe/src/servlet/SignupServlet.java
+++ MentorMe/src/servlet/SignupServlet.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.HashMap;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -25,20 +27,50 @@ public class SignupServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	/**
+	 * Adds student or tutor into the database.
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Hashmap of student and tutor data
+		HashMap studentInfo = new HashMap(); 
+		HashMap tutorInfo = new HashMap();
+		
+		// Either student or tutor
+		String type = request.getParameter("type");
+		
+		// Parameters common to both students and tutors
 		String firstName = request.getParameter("firstName");
 		String lastName = request.getParameter("lastName");
 		String email = request.getParameter("email");
 		String password = request.getParameter("password");
 		
-		System.out.println("First name: " + firstName);
-		// Need to write the SQL query and pass into DBController function
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		// Parameters specific to tutors
+		String school, degree, program;
+		
+		if (type.equals("student")) {
+			// Aggregte information into hashmap
+			studentInfo.put("firstName", firstName);
+			studentInfo.put("lastName", lastName);
+			studentInfo.put("email", email);
+			studentInfo.put("password", password);
+		} else {
+			// Get parameters specific to tutors
+			school = request.getParameter("school");
+			degree = request.getParameter("degree");
+			program = request.getParameter("program");
+			
+			// Aggregate information into hashmap
+			tutorInfo.put("firstName", firstName);
+			tutorInfo.put("lastName", lastName);
+			tutorInfo.put("email", email);
+			tutorInfo.put("password", password);
+			tutorInfo.put("school", school);
+			tutorInfo.put("degree", degree);
+			tutorInfo.put("program", program);
+		}
 	}
 
 }
