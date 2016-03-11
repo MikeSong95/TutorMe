2e1da230e2a91bc46fa20eafbf6dc3e0d81e55c5
Mo El Sayed <elsayed@ca.ibm.com>
2016-03-11 18:32:50 -0500

    added user classes

---
+++ MentorMe/src/com/user/Student.java
@@ -0,0 +1,29 @@
+package com.user;
+
+import java.util.HashMap;
+
+public class Student extends User {
+
+	private String schoolAttending;
+	private String programAttending;
+	private String[] coursesLearning= new String[5];
+	
+	public Student(HashMap user) {
+		
+		super(user);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
