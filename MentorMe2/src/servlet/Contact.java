6649959c100fa8f04fe4bebedfa2090866bb04d6
NickF <lkbjhgfd@hotmail.com>
2016-03-28 00:59:08 -0400

    Add email service

---
+++ MentorMe2/src/servlet/Contact.java
@@ -0,0 +1,80 @@
+package servlet;
+
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.activation.*;
+@WebServlet("/Contact")
+public class Contact extends HttpServlet{
+    
+  public void doGet(HttpServletRequest request,
+                    HttpServletResponse response)
+            throws ServletException, IOException
+  {
+      // Recipient's email ID needs to be mentioned.
+	  
+	  final String username = "mentormetest1@gmail.com";
+	  final String password = "new2you!";
+	  
+	  Properties props = new Properties();
+	  
+	    props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+      String to = "nick.r.feng@hotmail.com";
+ 
+      // Sender's email ID needs to be mentioned
+      String from = "mentormetest@outlook.com";
+ 
+      // Assuming you are sending email from localhost
+  
+ 
+      // Get the default Session object.
+      Session session = Session.getInstance(props,
+    		  new javax.mail.Authenticator() {
+    			protected PasswordAuthentication getPasswordAuthentication() {
+    				return new PasswordAuthentication(username, password);
+    			}
+    		  });
+      
+	  // Set response content type
+      response.setContentType("text/html");
+      PrintWriter out = response.getWriter();
+
+      try{
+         // Create a default MimeMessage object.
+         MimeMessage message = new MimeMessage(session);
+         // Set From: header field of the header.
+         message.setFrom(new InternetAddress(from));
+         // Set To: header field of the header.
+         message.addRecipient(Message.RecipientType.TO,
+                                  new InternetAddress(to));
+         // Set Subject: header field
+         message.setSubject("Inqury from user: " + request.getParameter("userName") + "email: " +request.getParameter("email"));
+         // Now set the actual message
+         String content = request.getParameter("message");
+         message.setText("Message: " +content);
+         // Send message
+         Transport.send(message);
+         String title = "Send Email";
+         String res = "Sent message successfully....";
+         String docType =
+         "<!doctype html public \"-//w3c//dtd html 4.0 " +
+         "transitional//en\">\n";
+         out.println(docType +
+         "<html>\n" +
+         "<head><title>" + title + "</title></head>\n" +
+         "<body bgcolor=\"#f0f0f0\">\n" +
+         "<h1 align=\"center\">" + title + "</h1>\n" +
+         "<p align=\"center\">" + res + "</p>\n" +
+         "</body></html>");
+      }catch (MessagingException mex) {
+         mex.printStackTrace();
+      }
+   }
+} 
